fded25808c9148261b7d8efa3ffbe0fc6f66fbc2
Doug Dryburgh <douglas.dryburgh@gmail.com>
2017-04-24 11:59:15 -0700

    front page complete

--- js/js.js
+++ js/js.js
@@ -14,47 +14,57 @@ $(document).ready(function () {
             }, 500);
         }
     });
-
-
     //QUOTE ANIMATION
-    var quotes = ["The greatest thing to happen to me since my 3rd wife.", "this is quote 2", "this is the last quote homie"];
-
+    var quotes = ['"The greatest thing to happen to me since my 3rd wife."', '"I\'ve been a groupie since the beginning"', '"I never thought I\'d find a band I liked more than Nickleback"'];
     var i = 0;
 
     function quoteRotation() {
-        console.log("running");
-        console.log(i);
-
         $("#quote > h2").html(quotes[i]);
-
         $("#quote").animate({
             right: "0px",
         }, 1000);
-
         setTimeout(function () {
             $("#quote").animate({
-                right: "-1100px",
+                right: "-2000px",
             }, 1000);
-        }, 5000);
-
-        if (i === 2) {
+        }, 6000);
+        if (i === quotes.length - 1) {
             i = 0;
         } else {
             i++;
         }
-    
+    }
+   setTimeout(function () {
+        quoteRotation();
+        setTimeout(function () {
+            setInterval(quoteRotation, 8000)
+        })
+    }, 500);
 
+    //BACKGROUND ANIMATION
+    var backgrounds = ["url(./images/image1.jpeg)", "url(./images/image2.jpeg)", "url(./images/image3.jpeg)", "url(./images/image4.jpeg)", "url(./images/image5.jpeg)"];
+    var j = 0;
+
+    function backgroundRotation() {
+        $("#backgroundImage").css("background-image", backgrounds[j]);
+        $("#backgroundImage").animate({
+            opacity: "1",
+        }, 1000);
+
+        setTimeout(function () {
+            $("#backgroundImage").animate({
+                opacity: "0",
+            }, 1000)
+        }, 7000);
+        if (j === backgrounds.length - 1) {
+            j = 0;
+        } else {
+            j++;
+        }
     }
 
-    setInterval(quoteRotation, 7000);
+    backgroundRotation();
+setInterval(backgroundRotation, 8000);
 
 
-
-    /*    if (i <= quotes.length-1) {
-            i++;
-        }
-        else {
-            i = 0;
-        }
-*/
 });
